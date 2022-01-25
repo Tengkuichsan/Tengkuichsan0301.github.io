b403a6686ce5b56d0fc659f1242514ae7a00b80a
Tengkuichsan <96951421+Tengkuichsan@users.noreply.github.com>
2022-01-26 01:33:38 +0700

    Add files via upload

---
+++ Muncul.js
@@ -0,0 +1,9 @@
+let myNumber = 16;
+let myString = "Selamat Datang Di Website Kami"
+let myBoolean = true
+const phi = 3.14
+
+console.log(myNumber)
+console.log(myString)
+console.log(myBoolean)
+console.log(phi)
